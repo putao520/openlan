67d91a2d5def1a60e825c701cf58737e022832db
Daniel Ding <danieldin95@163.com>
2019-08-19 02:12:36 -0400

    update.

--- olv2/endpoint/config.go
+++ olv2/endpoint/config.go
@@ -31,7 +31,7 @@ func NewConfig() (this *Config) {
     flag.BoolVar(&this.Verbose, "verbose", false, "open verbose")
     flag.StringVar(&this.UdpListen, "udp", "0.0.0.0:10010",  "the udp listen on")
 	flag.StringVar(&this.Controller, "ctl", "openlan.net:10020",  "the controller listen on")
-	flag.StringVar(&this.HttpListen, "http", "0.0.0.0:10082",  "the http listen on")
+	flag.StringVar(&this.HttpListen, "http", "0.0.0.0:10087",  "the http listen on")
 	flag.StringVar(&this.Auth, "auth", "default@openlan:", "the authentication login")
 	flag.IntVar(&this.Ifmtu, "ifmtu", 1430, "the interface MTU include ethernet") //1500-20-8-20-14 ~ 1430 for windows.
 	flag.IntVar(&this.Interval, "interval", 10, "the interval heartbeat to controller") 
