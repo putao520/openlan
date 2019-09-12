87fae4800197f9dab0f224bca1caf26a4806a671
Daniel Ding <danieldin95@163.com>
2019-09-11 23:15:24 -0400

    update.

--- olv1/install.sh
+++ olv1/install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-iptables -I INPUT -p tcp --dport 10002 -j ACCEPT
-iptables -I INPUT -p tcp --dport 10082 -j ACCEPT
+firewall-cmd --zone=public --add-port=10002/tcp --permanent
+firewall-cmd --zone=public --add-port=10082/tcp --permanent
+firewall-cmd --reload
 
+#iptables -I INPUT -p tcp --dport 10002 -j ACCEPT
+#iptables -I INPUT -p tcp --dport 10082 -j ACCEPT
 #iptables -t nat -I PREROUTING -d 117.89.132.47 -p tcp -m tcp --dport 10002 -j DNAT --to-destination 192.168.4.151:10002
 #iptables -t nat -I PREROUTING -d 117.89.132.47 -p tcp -m tcp --dport 10082 -j DNAT --to-destination 192.168.4.151:10082
 
