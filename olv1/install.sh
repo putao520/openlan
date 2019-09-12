398341b6e0d1ad2ac6b5a0200a89e37b4006a445
Daniel Ding <danieldin95@163.com>
2019-09-11 23:21:06 -0400

    update.

--- olv1/install.sh
+++ olv1/install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-firewall-cmd --zone=public --add-port=10002/tcp --permanent
-firewall-cmd --zone=public --add-port=10082/tcp --permanent
-firewall-cmd --reload
+systemctl status firewalld && {
+  firewall-cmd --zone=public --add-port=10002/tcp --permanent
+  firewall-cmd --zone=public --add-port=10082/tcp --permanent
+  firewall-cmd --reload
+}
 
-#iptables -I INPUT -p tcp --dport 10002 -j ACCEPT
-#iptables -I INPUT -p tcp --dport 10082 -j ACCEPT
-#iptables -t nat -I PREROUTING -d 117.89.132.47 -p tcp -m tcp --dport 10002 -j DNAT --to-destination 192.168.4.151:10002
-#iptables -t nat -I PREROUTING -d 117.89.132.47 -p tcp -m tcp --dport 10082 -j DNAT --to-destination 192.168.4.151:10082
+[ -e '/usr/sbin/iptables' ] && { 
+  echo ""
+  #iptables -I INPUT -p tcp --dport 10002 -j ACCEPT
+  #iptables -I INPUT -p tcp --dport 10082 -j ACCEPT
+
+  #iptables -t nat -I PREROUTING -d 117.89.132.47 -p tcp -m tcp --dport 10002 -j DNAT --to-destination 192.168.4.151:10002
+  #iptables -t nat -I PREROUTING -d 117.89.132.47 -p tcp -m tcp --dport 10082 -j DNAT --to-destination 192.168.4.151:10082
+}
 
 cp -rvf ./resources/point /usr/bin
 cp -rvf ./resources/vswitch /usr/bin
