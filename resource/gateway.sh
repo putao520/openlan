429cefaf15e01e70183fdd1f47ae0f7ada5e85d0
Daniel Ding <danieldin95@163.com>
2019-09-27 03:07:10 -0400

    udpate.

--- resource/gateway.sh
+++ resource/gateway.sh
@@ -5,7 +5,7 @@ yourNet="192.168.4.0/24"
 privateGw="192.168.10.20"
 publicIp="185.243.43.127"
 
-/usr/bin/ip route add $yourNet via $privateGw
+/usr/sbin/ip route add $yourNet via $privateGw
 
 [ -e "/usr/bin/firewall-cmd" ] && {
   /usr/bin/firewall-cmd --zone=public --permanent --direct --passthrough ipv4 -t nat -I POSTROUTING -s $yourNet ! -d $yourNet -j SNAT --to-source $publicIp
