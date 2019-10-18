485ae45f061efdb7a44a60f2d0e0f2ba3caadc05
Daniel Ding <danieldin95@163.com>
2019-10-18 02:22:06 -0400

    update

--- vswitch/app/online.go
+++ vswitch/app/online.go
@@ -1,53 +1,27 @@
 package app
 
 import (
-	"fmt"
 	"github.com/lightstar-dev/openlan-go/libol"
 	"github.com/lightstar-dev/openlan-go/vswitch/api"
 	"github.com/lightstar-dev/openlan-go/vswitch/models"
-	"net"
 	"sync"
 )
 
-type Line struct {
-	EthType    uint16
-	IpSource   net.IP
-	IPDest     net.IP
-	IpProtocol uint8
-	PortDest   uint16
-	PortSource uint16
-}
-
-func NewLine(t uint16) *Line {
-	l := &Line{
-		EthType: t,
-		IpSource:  nil,
-		IpProtocol: 0,
-		PortDest:  0,
-	}
-	return l
-}
-
-func (l *Line) String() string {
-	return fmt.Sprintf("%d:%s:%s:%d:%d",
-		l.EthType, l.IpSource, l.IPDest, l.IpProtocol, l.PortDest)
-}
-
 type Online struct {
 	lock   sync.RWMutex
-	lines  map[string]*Line
+	lines  map[string]*models.Line
 	worker api.Worker
 }
 
 func NewOnline(w api.Worker, c *models.Config) (o *Online) {
 	o = &Online{
-		lines:  make(map[string]*Line, 1024*4),
+		lines:  make(map[string]*models.Line, 1024*4),
 		worker: w,
 	}
 	return
 }
 
-func (o *Online) GetOnline(name string) *Line {
+func (o *Online) GetOnline(name string) *models.Line {
 	o.lock.RLock()
 	defer o.lock.RUnlock()
 
@@ -58,8 +32,8 @@ func (o *Online) GetOnline(name string) *Line {
 	return nil
 }
 
-func (o *Online) ListLine() <-chan *Line {
-	c := make(chan *Line, 1024)
+func (o *Online) ListLine() <-chan *models.Line {
+	c := make(chan *models.Line, 1024)
 
 	go func() {
 		o.lock.RLock()
@@ -97,7 +71,7 @@ func (o *Online) OnFrame(client *libol.TcpClient, frame *libol.Frame) error {
 		}
 		data = data[ip.Len:]
 
-		line := NewLine(eth.Type)
+		line := models.NewLine(eth.Type)
 		line.IpSource = ip.Source
 		line.IPDest = ip.Destination
 		line.IpProtocol = ip.Protocol
@@ -127,7 +101,7 @@ func (o *Online) OnFrame(client *libol.TcpClient, frame *libol.Frame) error {
 	return nil
 }
 
-func (o *Online) AddLine(line *Line) {
+func (o *Online) AddLine(line *models.Line) {
 	o.lock.Lock()
 	defer o.lock.Unlock()
 
