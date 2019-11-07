bea605f250b5c2071a6b0010c00164794b302954
Daniel Ding <danieldin95@163.com>
2019-11-07 05:38:23 -0500

    update

---
+++ vswitch/bridger_darwin.go
@@ -0,0 +1,33 @@
+package vswitch
+
+import (
+	"net"
+)
+
+type Bridger struct {
+	Ip   net.IP
+	Net  *net.IPNet
+	Mtu  int
+	Name string
+}
+
+func NewBridger(name string, mtu int) *Bridger {
+	b := &Bridger{
+		Name: name,
+		Mtu:  mtu,
+	}
+	return b
+}
+
+func (b *Bridger) Open(addr string) {
+	//TODO
+}
+
+func (b *Bridger) Close() {
+	//TODO
+}
+
+func (b *Bridger) AddSlave(name string) error {
+	//TODO
+	return nil
+}
