765678df9823c7829a4e0da26f93d48c5e4a0409
Daniel Ding <danieldin95@163.com>
2019-11-12 20:46:29 +0800

    Update point.windows.cmd

--- resource/point.windows.cmd
+++ resource/point.windows.cmd
@@ -1,15 +1,15 @@
 @ECHO off
 
 IF "%1%"=="before" (
-	ECHO "before running"
+	ECHO "do something before running"
 )
 
 IF "%1%"=="after" (
-	ECHO "after running"
+	ECHO "update route after running"
 	route ADD 192.168.10.0/24 192.168.4.151
 )
 
 IF "%1%"=="exit" (
-	ECHO "exited"
+	ECHO "clear route on exited"
 	route DELETE 192.168.10.0/24
 )
