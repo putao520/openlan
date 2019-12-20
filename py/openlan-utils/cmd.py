a9f5e219db5eba9c14e8ed4dc98d7234906bc8dc
Daniel Ding <danieldin95@163.com>
2019-12-20 10:36:22 -0500

    update

--- py/openlan-utils/cmd.py
+++ py/openlan-utils/cmd.py
@@ -1,5 +1,15 @@
 import json
+import argparse
+import os
 from .client import Client
+
+parse = argparse.ArgumentParser()
+parse.add_argument('--ol-token',
+                   help='Token series',
+                   default=os.environ.get("OL_TOKEN", ""))
+parse.add_argument('--ol-server',
+                   help='Server address',
+                   default=os.environ.get("OL_SERVER", "localhost:10000"))
 
 
 def with_client(func):
@@ -10,14 +20,35 @@ def with_client(func):
     return decorate
 
 
+subparsers = parse.add_subparsers()
+
+
+def cmd(sub_parser):
+    def decorate(func):
+        sub_parser.set_defaults(func=func)
+        return func
+
+    return decorate
+
+
+subpar = subparsers.add_parser('list-user', help="List all users")
+
+
+@cmd(subpar)
 @with_client
-def list_user(client, opt):
+def cmd_list_user(client, opt):
     resp = client.request("user", "GET")
     print json.dumps(resp.json(), indent=4)
 
 
+subpar = subparsers.add_parser('add-user', help="Add new user")
+subpar.add_argument("--username", help="Username")
+subpar.add_argument("--password", help="Password")
+
+
+@cmd(subpar)
 @with_client
-def add_user(client, opt):
+def cmd_add_user(client, opt):
     if opt.username is None or opt.password is None:
         return
 
@@ -29,8 +60,13 @@ def add_user(client, opt):
     print json.dumps(resp.json(), indent=4)
 
 
+subpar = subparsers.add_parser('del-user', help="Delete a user")
+subpar.add_argument("--username", help="Username")
+
+
+@cmd(subpar)
 @with_client
-def del_user(client, opt):
+def cmd_del_user(client, opt):
     if opt.username is None:
         return
 
@@ -40,3 +76,9 @@ def del_user(client, opt):
     resp = client.request("user", "DELETE", data)
 
     print resp.text
+
+
+def parse_args():
+    return parse.parse_args()
+
+
