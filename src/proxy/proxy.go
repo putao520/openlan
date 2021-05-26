3b8fa669832f49fa54e5273c50f6b235c3025923
sicheng <albert216@126.com>
2021-05-26 17:43:08 +0800

    support ss

--- src/proxy/proxy.go
+++ src/proxy/proxy.go
@@ -3,22 +3,25 @@ package proxy
 import (
 	"github.com/danieldin95/openlan-go/src/config"
 	"github.com/danieldin95/openlan-go/src/libol"
+	"github.com/danieldin95/openlan-go/src/proxy/ss"
 	"os"
 )
 
 type Proxy struct {
-	cfg   *config.Proxy
-	tcp   map[string]*TcpProxy
-	socks map[string]*SocksProxy
-	http  map[string]*HttpProxy
+	cfg    *config.Proxy
+	tcp    map[string]*TcpProxy
+	socks  map[string]*SocksProxy
+	http   map[string]*HttpProxy
+	shadow map[string]*ss.ShadowSocks
 }
 
 func NewProxy(cfg *config.Proxy) *Proxy {
 	return &Proxy{
-		cfg:   cfg,
-		socks: make(map[string]*SocksProxy, 32),
-		tcp:   make(map[string]*TcpProxy, 32),
-		http:  make(map[string]*HttpProxy, 32),
+		cfg:    cfg,
+		socks:  make(map[string]*SocksProxy, 32),
+		tcp:    make(map[string]*TcpProxy, 32),
+		http:   make(map[string]*HttpProxy, 32),
+		shadow: make(map[string]*ss.ShadowSocks, 32),
 	}
 }
 
@@ -43,6 +46,13 @@ func (p *Proxy) Initialize() {
 		h := NewHttpProxy(c)
 		p.http[c.Listen] = h
 	}
+	for _, c := range p.cfg.Shadow {
+		if c == nil || c.Server == "" {
+			continue
+		}
+		h := ss.NewShadowSocks(c)
+		p.shadow[c.Server] = h
+	}
 }
 
 func (p *Proxy) Start() {
@@ -59,6 +69,9 @@ func (p *Proxy) Start() {
 	for _, h := range p.http {
 		h.Start()
 	}
+	for _, s := range p.shadow {
+		s.Start()
+	}
 }
 
 func (p *Proxy) Stop() {
@@ -68,6 +81,9 @@ func (p *Proxy) Stop() {
 	libol.Info("Proxy.Stop")
 	for _, t := range p.tcp {
 		t.Stop()
+	}
+	for _, s := range p.shadow {
+		s.Stop()
 	}
 }
 
