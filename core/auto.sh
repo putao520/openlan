c96834885e413a2f41284e9cfd7e3948e8e306f7
danieldin95 <danieldin95@163.com>
2022-03-15 00:49:48 +0800

    v5.9.1

--- core/auto.sh
+++ core/auto.sh
@@ -16,6 +16,16 @@ build_openvswitch() {
   }
 }
 
+check_and_update() {
+  file0=$1
+  file1=$2
+  md5f0=$(md5sum $file0 | awk '{print $1}')
+  md5f1=$(md5sum $file1 | awk '{print $1}')
+  if [ "$md5f0"x != "$md5f1"x ]; then
+    mv $file0 $file1
+  fi
+}
+
 clean_openvswitch() {
   cd ovs && {
     if [ -e Makefile ]; then
@@ -32,9 +42,12 @@ type $python_bin || python_bin="python3"
 build_idlc() {
   idlc_bin="ovs/ovsdb/ovsdb-idlc.in"
   [ -e "idlc/confd.ovsschema" ] || ln -s -f ../../dist/resource/confd.schema.json idlc/confd.ovsschema
-  PYTHONPATH="ovs/python:"$PYTHONPATH PYTHONDONTWRITEBYTECODE=yes $python_bin $idlc_bin annotate idlc/confd.ovsschema idlc/confd-idl.ann > idlc/confd-idl.ovsidl
-  PYTHONPATH="ovs/python:"$PYTHONPATH PYTHONDONTWRITEBYTECODE=yes $python_bin $idlc_bin c-idl-source idlc/confd-idl.ovsidl > idlc/confd-idl.c
-  PYTHONPATH="ovs/python:"$PYTHONPATH PYTHONDONTWRITEBYTECODE=yes $python_bin $idlc_bin c-idl-header idlc/confd-idl.ovsidl > idlc/confd-idl.h
+  PYTHONPATH="ovs/python:"$PYTHONPATH PYTHONDONTWRITEBYTECODE=yes $python_bin $idlc_bin annotate idlc/confd.ovsschema idlc/confd-idl.ann > /tmp/confd-idl.ovsidl
+  check_and_update /tmp/confd-idl.ovsidl idlc/confd-idl.ovsidl
+  PYTHONPATH="ovs/python:"$PYTHONPATH PYTHONDONTWRITEBYTECODE=yes $python_bin $idlc_bin c-idl-source idlc/confd-idl.ovsidl > /tmp/confd-idl.c
+  check_and_update /tmp/confd-idl.c idlc/confd-idl.c
+  PYTHONPATH="ovs/python:"$PYTHONPATH PYTHONDONTWRITEBYTECODE=yes $python_bin $idlc_bin c-idl-header idlc/confd-idl.ovsidl > /tmp/confd-idl.h
+  check_and_update /tmp/confd-idl.h idlc/confd-idl.h
 }
 
 update_version() {
