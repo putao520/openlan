00a9fd77d554ec1f76bd7a0aaab71b6ba08eb875
danieldin95 <danieldin95@163.com>
2022-03-25 21:06:36 +0800

    fix: update playbook.

--- dist/script/update.sh
+++ dist/script/update.sh
@@ -2,10 +2,13 @@
 
 set -ex
 
+## Upgrade ovsdb
+# ovsdb-client convert unix:///var/openlan/confd.sock /etc/openlan/switch/confd.schema.json
+
+## Enable check for DDNS
 # [root@centos ~]# crontab -l
 # 0,5,10,15,20,25,30,35,40,45,50,55 * * * * /var/openlan/script/update.sh
 # [root@centos ~]#
 
-export OL_VERSION=v6
-
-# /usr/bin/openlan name add --name your.ddns.name
+## Add your DDNS
+# OL_VERSION=v6 /usr/bin/openlan name add --name your.ddns.name
